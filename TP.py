055d6fa86050c503759e91e899e878c56f2b22a9
François Illien <francois@illien.org>
2025-03-12 14:22:57 +0100

    uncomment Step 3
    Signed-off-by: François Illien <francois@illien.org>

--- TP.py
+++ TP.py
@@ -11,8 +11,8 @@ from dev_A_tools import greet
 print(greet("Dev.A"))
 
 # Step 3: Random fun
-# import random
-# print("Lucky number:", random.randint(1, 100))
+import random
+print("Lucky number:", random.randint(1, 100))
 
 # Step 4: Final message
 # print("Great job completing the steps!")
